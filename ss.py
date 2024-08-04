999f0e4549bd4762c486007695bec44d71e938be
metalhead <pittore086+git@yandex.ru>
2024-08-04 16:22:57 +0500

    002

--- ss.py
+++ ss.py
@@ -1,1 +1,20 @@
-new
+import pandas as pd
+import random
+
+# Генерация данных
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# Получение уникальных значений из столбца
+unique_values = data['whoAmI'].unique()
+
+# Создание новых столбцов для каждой уникальной категории
+for value in unique_values:
+    data[value] = (data['whoAmI'] == value).astype(int)
+    
+# Удаление исходного столбца
+data = data.drop(columns=['whoAmI'])
+
+print(data.head())
